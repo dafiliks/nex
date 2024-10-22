e059a9a431e9706cf7f872acf24e2aa3192492d2
David Filiks <dafiliks@gmail.com>
2024-10-22 15:52:22 +0000

    First commit, starting it up

---
+++ build/src/CMakeFiles/nex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nex.dir/main.cpp.o"
+  "CMakeFiles/nex.dir/main.cpp.o.d"
+  "nex"
+  "nex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
